448330bf5b1968607c141f8afc79d334af3aac43
Jakob <i-jakob.dekleva@devrev.ai>
2024-11-04 09:08:31 +0100

    Fix: test name

--- build.zig
+++ build.zig
@@ -51,7 +51,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/tes.zig"),
+        .root_source_file = b.path("src/test.zig"),
         .target = target,
         .optimize = optimize,
     });
